bd75f2471960ff1516380eafc3065013daa54103
Pierre Beaujean <pierreb24@gmail.com>
2018-11-13 15:20:53 +0100

    use pipenv

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 import qcip_tools
 from setuptools import setup
-try: # for pip >= 10
+try:  # for pip >= 10
     from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
+except ImportError:  # for pip <= 9.0.3
     from pip.req import parse_requirements
 
 pkgs = []
 dependency_links = []
-for pkg in parse_requirements('requirements.txt', session=False):
-    if pkg.req:
+for pkg in parse_requirements('.install_require', session=False):
+    if pkg.link:
+        dependency_links.append(str(pkg.link))
+    else:
         pkgs.append(str(pkg.req))
 
 setup(
